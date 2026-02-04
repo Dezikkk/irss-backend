faf67f5d0e6f0b7c3b0ac01a480b9699577134d0
Dezik <dawparuzel@gmail.com>
2026-02-04 22:55:44 +0100

    small tweaks

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -201,5 +201,5 @@ async def verify_token(token: str, db: SessionDep):
     # )
     
     return RedirectResponse(
-        url=f"{settings.FRONTEND_URL}/auth_callback.html#access_token={access_token}"
+        url=f"{settings.FRONTEND_URL}/index?access_token={access_token}"
     )
