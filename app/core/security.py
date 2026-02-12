06df956cd1b3abfdef557a254d511b43182e4a2b
auregosu <templariopersario@outlook.com>
2026-02-12 16:16:10 +0100

    rollback to smtp, discarded resend

--- app/core/security.py
+++ app/core/security.py
@@ -5,7 +5,6 @@ from email.message import EmailMessage
 from typing import Optional
 from jose import jwt
 from fastapi import HTTPException
-import resend
 
 from app.config import get_settings
 
@@ -23,26 +22,35 @@ async def send_magic_link_email(email: str, token: str, invite: str = None):
     if invite:
         magic_link += f"&invite={invite}"
 
-    html = f"""
-    <h2>Logowanie do {settings.APP_NAME}</h2>
-    <p>Kliknij w link poniżej, aby się zalogować:</p>
-    <p>
-        <a href="{magic_link}">Zaloguj się</a>
-    </p>
-    <p>Link wygaśnie za {settings.TOKEN_EXPIRE_MINUTES} minut.</p>
-    <br/>
-    <p>Pozdro,<br>{settings.APP_NAME}</p>
-    """
+    message = EmailMessage()
+    message["From"] = settings.SMTP_FROM
+    message["To"] = email
+    message["Subject"] = f"{settings.APP_NAME} - Magic Link"
+
+    message.set_content(f'''
+        <h2>Logowanie do {settings.APP_NAME}</h2>
+        <p>Kliknij w link poniżej, aby się zalogować:</p>
+        <p>Jeżeli nie jesteś {email} to... Coś poszło nie tak...</p>
+        <p>A jak tak to fajnie!</p>
+        <p>
+            <a href="{magic_link}">Zaloguj się</a>
+        </p>
+        <p>Link wygaśnie za {settings.TOKEN_EXPIRE_MINUTES} minut.</p>
+        <br/>
+        <p>Pozdro,<br>{settings.APP_NAME}</p>
+    ''')
 
     try:
-        resend.Emails.send({
-            "from": settings.RESEND_EMAIL_FROM,
-            # "to": email,
-            #"to": settings.SMTP_USER, # debug test potem to zamien
-            "to": email,
-            "subject": f"{settings.APP_NAME} - Magic Link",
-            "html": html
-        })
+        await aiosmtplib.send(
+            message,
+            hostname=settings.SMTP_HOST,
+            port=settings.SMTP_PORT,
+            username=settings.SMTP_USER,
+            password=settings.SMTP_PASSWORD,
+            start_tls=True,
+            # start_tls=True
+            timeout=10
+        )
     except Exception as e:
         raise HTTPException(
             status_code=500,
